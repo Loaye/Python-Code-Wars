2cd07f430722b719f8e4f20406ef6a82208eff30
Loaye <jr.carstens00@gmail.com>
2017-10-21 14:43:47 -0700

    problem 2 - kata 8

--- day_of_code/problem_one/middle_character.py
+++ day_of_code/problem_one/middle_character.py
@@ -8,9 +8,9 @@ def get_middle(s):
 
 """
 
+
 def get_middle(s):
     if len(s) % 2 == 0:
-        return s[int(len(s) / 2) - 1] + s[int(len(s) / 2)]      
+        return s[int(len(s) / 2) - 1] + s[int(len(s) / 2)]
     else:
         return s[int(len(s) / 2)]
-        
---
+++ day_of_code/problem_two/calculate_avg.py
@@ -0,0 +1,14 @@
+"""
+Kata : Calculate Average
+
+#1 Best Practices Solution by mirat, Malavolent, tedmiston, riyakayal, tachyonlabs, Parabellum117 (plus 108 more warriors)
+
+def find_average(array):
+    return sum(array) / len(array) if array else 0
+
+"""
+
+def find_average(array):
+    if len(array) == 0:
+        return 0
+    return sum(array) / len(array)
